1e12fd2e353e4023b21405e10984bff74682a0bd
VinitaRane05 <ranevinita32@gmail.com>
2025-11-13 01:36:40 +0530

    Add files via upload

---
+++ remedies3.py
@@ -0,0 +1,10 @@
+# remedies.py
+REMEDIES = {
+    "Late blight": "Remove infected leaves. Apply copper fungicide. Avoid overhead watering.",
+    "Early blight": "Rotate crops, prune infected areas, and apply sulfur spray if needed.",
+    "Powdery mildew": "Use neem oil or potassium bicarbonate weekly.",
+    "Leaf spot": "Remove infected leaves and apply a mild fungicide.",
+    "Rust": "Use sulfur-based spray and remove diseased leaves.",
+    "Bacterial wilt": "Improve drainage and use resistant plant varieties.",
+    "Healthy": "Your plant looks good! Keep soil balanced and hydrated."
+}
